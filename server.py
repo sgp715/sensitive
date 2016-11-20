39025d0ea076accbd1730399c4d7ddb1d1ae74e7
Ubuntu <ubuntu@ip-172-31-28-167.ec2.internal>
2016-11-20 04:38:38 +0000

    hacked youtube

---
+++ server.py
@@ -0,0 +1,64 @@
+import json
+
+import flask
+import httplib2
+
+from googleapiclient.discovery import build
+from oauth2client import client
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+  if 'credentials' not in flask.session:
+    return flask.redirect(flask.url_for('oauth2callback'))
+  credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+  if credentials.access_token_expired:
+    return flask.redirect(flask.url_for('oauth2callback'))
+  else:
+    http_auth = credentials.authorize(httplib2.Http())
+    youtube = build('youtube', 'v3', http_auth) #, developerKey='AIzaSyBwDLWOayjBDmFXU-QsCoGFc0l6pIWFfYs')
+    response = youtube.commentThreads().list(
+        part="snippet",
+        allThreadsRelatedToChannelId="UCW4C_qVMMbidqni7noNNZDg",
+        textFormat="plainText"
+    ).execute()
+    return json.dumps(response)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+  flow = client.flow_from_clientsecrets(
+      'client_secrets.json',
+      scope='https://www.googleapis.com/auth/youtube.force-ssl',
+      redirect_uri=flask.url_for('oauth2callback', _external=True))
+      #include_granted_scopes=True)
+  if 'code' not in flask.request.args:
+    auth_uri = flow.step1_get_authorize_url()
+    return flask.redirect(auth_uri)
+  else:
+    auth_code = flask.request.args.get('code')
+    credentials = flow.step2_exchange(auth_code)
+    flask.session['credentials'] = credentials.to_json()
+    return flask.redirect(flask.url_for('index'))
+
+
+if __name__ == '__main__':
+  import uuid
+  from optparse import OptionParser
+
+  app.secret_key = str(uuid.uuid4())
+
+  parser = OptionParser()
+
+  parser.add_option("-p", "--prod",
+                action="store_true", dest="prod", default=False,
+                help="don't print status messages to stdout")
+  (options, args) = parser.parse_args()
+
+  if options.prod == True:
+      app.run(host='0.0.0.0',port=80)
+  else:
+      app.run(use_debugger=True, debug=True,
+    use_reloader=True)
